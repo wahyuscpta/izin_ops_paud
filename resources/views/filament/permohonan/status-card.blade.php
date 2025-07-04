089525ea0dd3e97acf3d1a1125e10cafed4627e0
Wahyu Sucipta <wahyuscpta13@gmail.com>
2025-07-04 20:44:50 +0800

    [fixing] fix btn tanggal kunjungan

--- resources/views/filament/permohonan/status-card.blade.php
+++ resources/views/filament/permohonan/status-card.blade.php
@@ -39,7 +39,7 @@
                 </p>
             </div>
 
-            @if ($record->user->hasRole('admin'))
+            @if (Auth::user()->hasRole('admin'))
                 <x-filament::button
                     size="sm"
                     color="primary"
