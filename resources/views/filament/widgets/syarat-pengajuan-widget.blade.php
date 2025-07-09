efe26f1cca3d3d0cf050e0b518e95424cb5ad10d
Wahyu Sucipta <wahyuscpta13@gmail.com>
2025-07-10 01:30:11 +0800

    [feat] add dokumen syarat

--- resources/views/filament/widgets/syarat-pengajuan-widget.blade.php
+++ resources/views/filament/widgets/syarat-pengajuan-widget.blade.php
@@ -15,8 +15,8 @@
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Struktur Lembaga Kursus/PAUD</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/struktur_organisasi.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
@@ -26,136 +26,128 @@
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Daftar Sarana dan Prasarana Lembaga PAUD/Kursus</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/sarpras.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Kurikulum Kursus/PAUD</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/kurikulum.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Tata Tertib Kursus/PAUD</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/tata_tertib.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Peta Lokasi Kursus/PAUD</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/peta.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Daftar Peserta Didik Kursus/PAUD</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/peserta_didik.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Daftar Guru/Pendidik</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/instruktur.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Akte Notaris/Pendirian Yayasan dan Kemenhumham</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/akte_pendiri_yayasan.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Surat Permohonan Rekomendasi ke Lurah/Kepala Desa</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/permohonan_ke_lurah.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Surat Rekomendasi dari Lurah/Kepala Desa</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/lurah_ke_lembaga.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Surat permohonan Rekomendasi ke Korwil Disdikpora Setempat</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
-                        </a>                        
-                    </div>
-                </li>
-                <li class="text-sm">
-                    <div class="flex gap-3">
-                        <span>Surat Permohonan Rekomendasi ke Korwil Disdikpora Setempat</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/permohonan_ke_korwil.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Surat Rekomendasi dari Korwil Disdikpora Setempat</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/korwil_ke_dinas.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Surat Permohonan Ditujukan Kepada Kepala Dinas</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/permohonan_ke_kepala_dinas.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>RIP (Rencana Induk Pengembangan)</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/rip.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
-                        <span>IMB (Ijin Mendirikan Bangunan)</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <span>IMB (Izin Mendirikan Bangunan)</span>
+                        <a href="{{ asset('ex-lampiran/imb.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>Perjanjian sewa menyewa</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/perjanjian_sewa.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
                 <li class="text-sm">
                     <div class="flex gap-3">
                         <span>NIB (No Induk Berusaha)</span>
-                        <a href="#" target="_blank" class="inline-block">
-                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 3px" />
+                        <a href="{{ asset('ex-lampiran/nib.pdf') }}" target="_blank" class="inline-block">
+                            <x-filament::icon icon="heroicon-o-document-arrow-down" class="w-4 h-4 cursor-pointer text-primary-600" style="margin-top: 2px" />
                         </a>                        
                     </div>
                 </li>
